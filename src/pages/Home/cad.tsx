42404cbc0516faf02dfe7dfe8a2fc0cd5d858181
Adonis Joseph <magic.front.68@gmail.com>
2024-12-04 07:36:05 -0800

    white line fixed

--- src/pages/Home/cad.tsx
+++ src/pages/Home/cad.tsx
@@ -12,7 +12,7 @@ const TaskCard: React.FC<TaskCardProps> = ({ title, description, reward, timeLef
     <div className="bg-cover bg-center bg-no-repeat bg-[url('/images/background/job_item_bg.png')] text-white w-full h-full p-4 rounded-lg shadow-md mb-3 w-90">
       <h2 className="text-lg font-semibold mb-2 truncate">{title}</h2>
       <p className="text-lg text-[#b0b3c1] mb-4">{description}</p>
-      <img className="mb-3" src="/images/white_line.png" />
+      <img className="mb-3 w-full" src="/images/white_line.png" />
       <div className="flex justify-center items-center">
         <div className="flex items-center pr-20">
           <img src="/images/money.png" alt="Coins" className="w-4 h-4 mr-1" />
